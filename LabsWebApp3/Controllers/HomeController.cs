76fe6eba26c80e96651aa3ed5da4ef23989062cf
vovikdoc <vovikdoc@yandex.ru>
2020-10-17 08:19:28 +0700

    #LAB 4.0.1 Другой способ добавления страницы
    Добавлена PrivacyPage

--- LabsWebApp3/Controllers/HomeController.cs
+++ LabsWebApp3/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using LabsWebApp3.Models.Domain;
+using System;
+using LabsWebApp3.Models.Domain.Entities;
 
 namespace XandCo.Controllers
 {
@@ -20,6 +22,23 @@ namespace XandCo.Controllers
         public IActionResult Contacts()
         {
             return View("TextOnly", dataManager.TextFields.GetItemByCodeWord("ContactsPage"));
+        }
+        public IActionResult Privacy()
+        {
+            if (dataManager.TextFields.GetItemByCodeWord("PrivacyPage") == null)
+                dataManager.TextFields.SaveItem(new TextField
+                {
+                    CodeWord = "PrivacyPage",
+                    Title = "Соглашения",
+                    Text = "<h2 align=\"center\">Соглашения</h2>" +
+                    "<h1>Заходя на этот сайт, Вы соглашаетесь со следующим:</h1>" +
+                        "<h3>8.  Информация о Ваших банковских картах, включая CVR коды, будут украдены, " +
+                            "деньги в последствии будут сняты и пропиты разработчиками данного сайта.</h3>" +
+                        "<h3>9.  Пароли к Вашим социальным сетям будут взломаны и отданы младшим родственникам и знакомым разработчиков для развлечений.</h3>" +
+                        "<h3>10.  Ещё с семью пунктами, который были сознательно скрыты в этом соглашении.</h3>" +
+                    "<h6 align=\"center\">Praemonitus, praemunitus</h6>"
+                });
+            return View("TextOnly", dataManager.TextFields.GetItemByCodeWord("PrivacyPage"));
         }
     }
 }
