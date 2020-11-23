c94af117319dc37e5ae8e7b88fee4ab15aef87ce
Vladimir <vovikdoc@gmail.com>
2020-11-23 13:48:23 +0700

    6.02

--- SimpleSocket/SocketServer/Program.cs
+++ SimpleSocket/SocketServer/Program.cs
@@ -9,16 +9,22 @@ namespace SocketServer
         static void Main(string[] args)
         {
             TcpListener server = new TcpListener(IPAddress.Any, 4010);
+            
             server.Start();
             Console.WriteLine("server.Start " + server.LocalEndpoint);
             
             TcpClient client = server.AcceptTcpClient();
             Console.WriteLine("AcceptTcpClient " + client);
 
+            var stream = client.GetStream();
+            byte[] buffer = {55, 56};
+            stream.Write(buffer , 0, buffer.Length);
+
             client.Close();
             Console.WriteLine("client closed");
             server.Stop();
             Console.WriteLine("server stopped");
+            
             Console.ReadLine();
         }
     }
