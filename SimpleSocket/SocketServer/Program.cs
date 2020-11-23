b9faed4ebc2f78a06e227fe1840d3c9476eedfd3
Vladimir <vovikdoc@gmail.com>
2020-11-23 10:47:27 +0700

    #LAB 6.0 Socket init project

---
+++ SimpleSocket/SocketServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace SocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
